9dc3fe9e757d6370da9468aba6d17f14243edbe6
omar bazeed <omarbazeed@gmail.com>
2023-05-03 23:08:17 +0200

    New Update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     MONGO_URI: "mongodb+srv://omarbazeed:gH1k82RvuwGyK03n@cluster0.nd1du9e.mongodb.net/?retryWrites=true&w=majority",
     APP_DEV: "http://localhost:3000",
-    APP_PROD: "https://nextjs-e-commerce.onrender.com",
+    APP_PROD: "https://nextjs-e-commerce.vercel.app",
   },
   images: {
     domains: ["images.pexels.com"],
